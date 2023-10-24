c0456c632edbc1cd4afd1f7386547a63e1c3868e
VN Phạm <nam.pv1805@gmail.com>
2023-10-25 00:20:24 +0700

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('Auxo server')
+        .setDescription('Server for auxo project')
+        .setVersion('0.0.1')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    await app.listen(3000);
+}
+bootstrap();
