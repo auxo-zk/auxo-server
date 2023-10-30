b000597ee86a4da5a332fea148cd3f12d77f99f2
VN Phạm <nam.pv1805@gmail.com>
2023-10-30 15:35:57 +0700

    Add task scheduler for Mina module

--- src/mina/mina.module.ts
+++ src/mina/mina.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { Network } from './network/network';
 import { TasksService } from './tasks/tasks.service';
 import { QueryService } from './query/query.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
+    imports: [ScheduleModule.forRoot()],
     providers: [Network, TasksService, QueryService],
 })
 export class MinaModule {}
