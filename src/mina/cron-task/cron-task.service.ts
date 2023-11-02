c211cf77ce715934ad3d1e9f665fc680fbb68286
VN Phạm <nam.pv1805@gmail.com>
2023-11-02 15:25:34 +0700

    Update CronTaskService

--- src/mina/cron-task/cron-task.service.ts
+++ src/mina/cron-task/cron-task.service.ts
@@ -2,25 +2,29 @@ import { Injectable, Logger } from '@nestjs/common';
 import { Cron, Interval } from '@nestjs/schedule';
 import { fetchLastBlock } from 'o1js';
 import { QueryService } from '../query/query.service';
+import { CommitteeService } from '../committee/committee.service';
 
 @Injectable()
 export class CronTaskService {
     private readonly logger = new Logger(CronTaskService.name);
 
-    constructor(private readonly queryService: QueryService) {}
+    constructor(
+        private readonly queryService: QueryService,
+        private readonly committeeService: CommitteeService,
+    ) {}
 
     @Cron('45 * * * * *')
-    handleCron() {
+    handleCron(): void {
         this.logger.debug('task scheduler');
     }
 
     @Interval(1800000)
-    async handleFetchMinaLastBlock() {
+    async handleFetchMinaLastBlock(): Promise<void> {
         await fetchLastBlock();
     }
 
     @Interval(1800000)
-    async handleFetchCommitteeEvents() {
-        await this.queryService.fetchEvents(process.env.COMMITTEE_ADDRESS);
+    async handleFetchCommitteeEvents(): Promise<void> {
+        await this.committeeService.fetchEvents();
     }
 }
