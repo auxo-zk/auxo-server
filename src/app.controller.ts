9c1fd8491ef06bf03ca3b3661a6ceef7b5cebbee
VN Phạm <nam.pv1805@gmail.com>
2024-01-20 15:42:03 +0700

    feat: Add api to reset contract collections

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,128 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ServerConfig } from './entities/server-config.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { CommitteeAction } from './schemas/actions/committee-action.schema';
+import { Model } from 'mongoose';
+import { Committee } from './schemas/committee.schema';
+import { DkgAction } from './schemas/actions/dkg-action.schema';
+import { Dkg } from './schemas/dkg.schema';
+import { Round1Action } from './schemas/actions/round-1-action.schema';
+import { Round1 } from './schemas/round-1.schema';
+import { Round2Action } from './schemas/actions/round-2-action.schema';
+import { Round2 } from './schemas/round-2.schema';
+import { Key } from './schemas/key.schema';
+import { RequestAction } from './schemas/actions/request-action.schema';
+import { RawDkgRequest } from './schemas/raw-request.schema';
+import { DkgRequest } from './schemas/request.schema';
+import { ResponseAction } from './schemas/actions/response-action.schema';
+import { DkgResponse } from './schemas/response.schema';
+import { ProjectAction } from './schemas/actions/project-action.schema';
+import { RawProject } from './schemas/raw-project.schema';
+import { Project } from './schemas/project.schema';
+import { CampaignAction } from './schemas/actions/campaign-action.schema';
+import { RawCampaign } from './schemas/raw-campaign.schema';
+import { Campaign } from './schemas/campaign.schema';
+import { ParticipationAction } from './schemas/actions/participation-action.schema';
+import { Participation } from './schemas/participation.schema';
+import { FundingAction } from './schemas/actions/funding-action.schema';
+import { Funding } from './schemas/funding.schema';
+import { FundingResult } from './schemas/funding-result.schema';
+import { TreasuryAction } from './schemas/actions/treasury-action.schema';
+import { Treasury } from './schemas/treasury.schema';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        @InjectModel(CommitteeAction.name)
+        private readonly committeeActionModel: Model<CommitteeAction>,
+        @InjectModel(Committee.name)
+        private readonly committeeModel: Model<Committee>,
+        @InjectModel(DkgAction.name)
+        private readonly dkgActionModel: Model<DkgAction>,
+        @InjectModel(Dkg.name)
+        private readonly dkgModel: Model<Dkg>,
+        @InjectModel(Round1Action.name)
+        private readonly round1ActionModel: Model<Round1Action>,
+        @InjectModel(Round1.name)
+        private readonly round1Model: Model<Round1>,
+        @InjectModel(Round2Action.name)
+        private readonly round2ActionModel: Model<Round2Action>,
+        @InjectModel(Round2.name)
+        private readonly round2Model: Model<Round2>,
+        @InjectModel(Key.name)
+        private readonly keyModel: Model<Key>,
+        @InjectModel(RequestAction.name)
+        private readonly requestActionModel: Model<RequestAction>,
+        @InjectModel(RawDkgRequest.name)
+        private readonly rawDkgRequestModel: Model<RawDkgRequest>,
+        @InjectModel(DkgRequest.name)
+        private readonly dkgRequestModel: Model<DkgRequest>,
+        @InjectModel(ResponseAction.name)
+        private readonly responseActionModel: Model<ResponseAction>,
+        @InjectModel(DkgResponse.name)
+        private readonly dkgResponseModel: Model<DkgResponse>,
+        @InjectModel(ProjectAction.name)
+        private readonly projectActionModel: Model<ProjectAction>,
+        @InjectModel(RawProject.name)
+        private readonly rawProjectModel: Model<RawProject>,
+        @InjectModel(Project.name)
+        private readonly projectModel: Model<Project>,
+        @InjectModel(CampaignAction.name)
+        private readonly campaignActionModel: Model<CampaignAction>,
+        @InjectModel(RawCampaign.name)
+        private readonly rawCampaignModel: Model<RawCampaign>,
+        @InjectModel(Campaign.name)
+        private readonly campaignModel: Model<Campaign>,
+        @InjectModel(ParticipationAction.name)
+        private readonly participationActionModel: Model<ParticipationAction>,
+        @InjectModel(Participation.name)
+        private readonly participationModel: Model<Participation>,
+        @InjectModel(FundingAction.name)
+        private readonly fundingActionModel: Model<FundingAction>,
+        @InjectModel(Funding.name)
+        private readonly fundingModel: Model<Funding>,
+        @InjectModel(FundingResult.name)
+        private readonly fundingResultModel: Model<FundingResult>,
+        @InjectModel(TreasuryAction.name)
+        private readonly treasuryActionModel: Model<TreasuryAction>,
+        @InjectModel(Treasury.name)
+        private readonly treasuryModel: Model<Treasury>,
+    ) {}
 
     @Get()
     getServerConfig(): ServerConfig {
         return this.appService.getServerConfig();
+    }
+
+    @Post('reset-contract-collections')
+    async resetContractCollections() {
+        await this.committeeActionModel.deleteMany({});
+        await this.committeeModel.deleteMany({});
+        await this.dkgActionModel.deleteMany({});
+        await this.dkgModel.deleteMany({});
+        await this.round1ActionModel.deleteMany({});
+        await this.round1Model.deleteMany({});
+        await this.round2ActionModel.deleteMany({});
+        await this.round2Model.deleteMany({});
+        await this.keyModel.deleteMany({});
+        await this.requestActionModel.deleteMany({});
+        await this.rawDkgRequestModel.deleteMany({});
+        await this.dkgRequestModel.deleteMany({});
+        await this.responseActionModel.deleteMany({});
+        await this.dkgResponseModel.deleteMany({});
+        await this.projectActionModel.deleteMany({});
+        await this.rawProjectModel.deleteMany({});
+        await this.projectModel.deleteMany({});
+        await this.rawCampaignModel.deleteMany({});
+        await this.campaignModel.deleteMany({});
+        await this.participationActionModel.deleteMany({});
+        await this.participationModel.deleteMany({});
+        await this.fundingActionModel.deleteMany({});
+        await this.fundingModel.deleteMany({});
+        await this.fundingResultModel.deleteMany({});
+        await this.treasuryActionModel.deleteMany({});
+        await this.treasuryModel.deleteMany({});
     }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -12,12 +12,120 @@ import { ApisModule } from './apis/apis.module';
 import { ObjectStorageService } from './object-storage/object-storage.service';
 import { ObjectStorageController } from './object-storage/object-storage.controller';
 
+import { Committee, CommitteeSchema } from 'src/schemas/committee.schema';
+import {
+    CommitteeAction,
+    CommitteeActionSchema,
+} from 'src/schemas/actions/committee-action.schema';
+import {
+    DkgAction,
+    DkgActionSchema,
+} from 'src/schemas/actions/dkg-action.schema';
+import {
+    Round1Action,
+    Round1ActionSchema,
+} from 'src/schemas/actions/round-1-action.schema';
+import {
+    Round2Action,
+    Round2ActionSchema,
+} from 'src/schemas/actions/round-2-action.schema';
+import { Key, KeySchema } from 'src/schemas/key.schema';
+import { Dkg, DkgSchema } from 'src/schemas/dkg.schema';
+import { Round1, Round1Schema } from 'src/schemas/round-1.schema';
+import { Round2, Round2Schema } from 'src/schemas/round-2.schema';
+import {
+    RequestAction,
+    RequestActionSchema,
+} from 'src/schemas/actions/request-action.schema';
+import {
+    ResponseAction,
+    ResponseActionSchema,
+} from 'src/schemas/actions/response-action.schema';
+import { DkgRequest, DkgRequestSchema } from 'src/schemas/request.schema';
+import { DkgResponse, DkgResponseSchema } from 'src/schemas/response.schema';
+import {
+    RawDkgRequest,
+    RawDkgRequestSchema,
+} from 'src/schemas/raw-request.schema';
+import {
+    ProjectAction,
+    ProjectActionSchema,
+} from 'src/schemas/actions/project-action.schema';
+import { RawProject, RawProjectSchema } from 'src/schemas/raw-project.schema';
+import { Project, ProjectSchema } from 'src/schemas/project.schema';
+import {
+    CampaignAction,
+    CampaignActionSchema,
+} from 'src/schemas/actions/campaign-action.schema';
+import { Campaign, CampaignSchema } from 'src/schemas/campaign.schema';
+import {
+    RawCampaign,
+    RawCampaignSchema,
+} from 'src/schemas/raw-campaign.schema';
+import {
+    ParticipationAction,
+    ParticipationActionSchema,
+} from 'src/schemas/actions/participation-action.schema';
+import {
+    Participation,
+    ParticipationSchema,
+} from 'src/schemas/participation.schema';
+import {
+    FundingAction,
+    FundingActionSchema,
+} from 'src/schemas/actions/funding-action.schema';
+import { Funding, FundingSchema } from 'src/schemas/funding.schema';
+import {
+    FundingResult,
+    FundingResultSchema,
+} from 'src/schemas/funding-result.schema';
+import {
+    TreasuryAction,
+    TreasuryActionSchema,
+} from 'src/schemas/actions/treasury-action.schema';
+import { Treasury, TreasurySchema } from 'src/schemas/treasury.schema';
+
 @Module({
     imports: [
         MongooseModule.forRoot(process.env.DB, {
             connectTimeoutMS: 10000000,
             socketTimeoutMS: 10000000,
         }),
+        MongooseModule.forFeature([
+            { name: CommitteeAction.name, schema: CommitteeActionSchema },
+            { name: Committee.name, schema: CommitteeSchema },
+            { name: DkgAction.name, schema: DkgActionSchema },
+            { name: Dkg.name, schema: DkgSchema },
+            { name: Round1Action.name, schema: Round1ActionSchema },
+            { name: Round1.name, schema: Round1Schema },
+            { name: Round2Action.name, schema: Round2ActionSchema },
+            { name: Round2.name, schema: Round2Schema },
+            { name: Key.name, schema: KeySchema },
+            { name: RequestAction.name, schema: RequestActionSchema },
+            { name: RawDkgRequest.name, schema: RawDkgRequestSchema },
+            { name: DkgRequest.name, schema: DkgRequestSchema },
+            { name: ResponseAction.name, schema: ResponseActionSchema },
+            { name: DkgResponse.name, schema: DkgResponseSchema },
+            { name: ProjectAction.name, schema: ProjectActionSchema },
+            { name: RawProject.name, schema: RawProjectSchema },
+            { name: Project.name, schema: ProjectSchema },
+            { name: CampaignAction.name, schema: CampaignActionSchema },
+            { name: RawCampaign.name, schema: RawCampaignSchema },
+            { name: Campaign.name, schema: CampaignSchema },
+            {
+                name: ParticipationAction.name,
+                schema: ParticipationActionSchema,
+            },
+            {
+                name: Participation.name,
+                schema: ParticipationSchema,
+            },
+            { name: FundingAction.name, schema: FundingActionSchema },
+            { name: Funding.name, schema: FundingSchema },
+            { name: FundingResult.name, schema: FundingResultSchema },
+            { name: TreasuryAction.name, schema: TreasuryActionSchema },
+            { name: Treasury.name, schema: TreasurySchema },
+        ]),
         MongooseModule.forFeature([]),
         MinaContractsModule,
         HttpModule,
