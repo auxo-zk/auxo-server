7d2ead7bec0d4e58e8eef5fb65c7e87635a7c18f
VN Phạm <nam.pv1805@gmail.com>
2024-04-25 17:02:58 +0700

    feat: update Requester Action Schema

---
+++ src/schemas/actions/requester-action.schema.ts
@@ -0,0 +1,96 @@
+import { Constants, ZkApp } from '@auxo-dev/dkg';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { Field } from 'o1js';
+import { Utilities } from 'src/mina-contracts/utilities';
+import { RawDkgRequest } from '../raw-request.schema';
+import { RequestActionEnum } from 'src/constants';
+
+export class RequesterActionData {
+    taskId: number;
+    timestamp: number;
+    keyIndex: number;
+    indices: string;
+    R: { x: string; y: string }[];
+    M: { x: string; y: string }[];
+    commitments: string[];
+
+    constructor(
+        taskId: number,
+        timestamp: number,
+        keyIndex: number,
+        indices: string,
+        R: { x: string; y: string }[],
+        M: { x: string; y: string }[],
+        commitments: string[],
+    ) {
+        this.taskId = taskId;
+        this.timestamp = timestamp;
+        this.keyIndex = keyIndex;
+        this.indices = indices;
+        this.R = R;
+        this.M = M;
+        this.commitments = commitments;
+    }
+
+    static fromAction(
+        action: ZkApp.Requester.RequesterAction,
+    ): RequesterActionData {
+        const R: { x: string; y: string }[] = [];
+        const M: { x: string; y: string }[] = [];
+        const commitments: string[] = [];
+        for (let i = 0; i < Constants.ENCRYPTION_LIMITS.DIMENSION; i++) {
+            R.push({
+                x: action.R.values[i].x.toString(),
+                y: action.R.values[i].y.toString(),
+            });
+            M.push({
+                x: action.M.values[i].x.toString(),
+                y: action.M.values[i].y.toString(),
+            });
+            commitments.push(action.commitments.values[i].toString());
+        }
+
+        return new RequesterActionData(
+            Number(action.taskId.toBigint()),
+            Number(action.timestamp.toBigInt()),
+            Number(action.keyIndex.toBigInt()),
+            action.indices.toString(),
+            R,
+            M,
+            commitments,
+        );
+    }
+}
+@Schema({ versionKey: false })
+export class RequesterAction {
+    @Prop({ required: true, unique: true, index: true, _id: true })
+    actionId: number;
+
+    @Prop({ required: true, unique: true })
+    currentActionState: string;
+
+    @Prop({ required: true, unique: true })
+    previousActionState: string;
+
+    @Prop()
+    actions: string[];
+
+    @Prop({ type: RequesterActionData })
+    actionData: RequesterActionData;
+
+    @Prop({ required: true, default: false })
+    active?: boolean;
+}
+
+export type RequesterActionDocument = HydratedDocument<RequesterAction>;
+export const RequesterActionSchema =
+    SchemaFactory.createForClass(RequesterAction);
+
+export function getRequesterActionData(actions: string[]): RequesterActionData {
+    const action = ZkApp.Requester.RequesterAction.fromFields(
+        Utilities.stringArrayToFields(actions),
+    );
+
+    return RequesterActionData.fromAction(action);
+}
