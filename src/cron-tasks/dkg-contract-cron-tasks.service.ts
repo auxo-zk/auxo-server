688f36019972fa12b4b7fbde1e15442f17e3323a
VN Phạm <nam.pv1805@gmail.com>
2024-06-03 22:12:22 +0700

    fix: update redis queue

--- src/cron-tasks/dkg-contract-cron-tasks.service.ts
+++ src/cron-tasks/dkg-contract-cron-tasks.service.ts
@@ -33,7 +33,11 @@ export class DkgContractCronTasksService implements OnModuleInit {
     @Cron('*/6 * * * *')
     async handleRollupContractsFirstOrder() {
         const jobCount = await this.contractServices.getJobCounts();
-        if (jobCount.active == 0 && jobCount.waiting == 0) {
+        if (
+            jobCount.active == 0 &&
+            jobCount.waiting == 0 &&
+            jobCount.delayed == 0
+        ) {
             this.logger.log(
                 'Register rollupContracts for the first order task at ' +
                     process.pid,
@@ -47,7 +51,11 @@ export class DkgContractCronTasksService implements OnModuleInit {
     @Cron('3,9,15,21,27,33,39,45,51,57 * * * *')
     async handleRollupContractsSecondOrder() {
         const jobCount = await this.contractServices.getJobCounts();
-        if (jobCount.active == 0 && jobCount.waiting == 0) {
+        if (
+            jobCount.active == 0 &&
+            jobCount.waiting == 0 &&
+            jobCount.delayed == 0
+        ) {
             this.logger.log(
                 'Register rollupContracts for the second order task at ' +
                     process.pid,
