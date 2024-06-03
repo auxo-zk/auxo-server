e40f76209ae56f1006bb4f8f2b62dce684f06f5c
VN Phạm <nam.pv1805@gmail.com>
2024-06-03 22:44:16 +0700

    fix: update redis queue

--- src/cron-tasks/dkg-contract-cron-tasks.service.ts
+++ src/cron-tasks/dkg-contract-cron-tasks.service.ts
@@ -15,7 +15,7 @@ export class DkgContractCronTasksService implements OnModuleInit {
     async onModuleInit() {
         this.logger.log('Register compileContracts task at ' + process.pid);
         await this.contractServices.client.flushdb();
-        this.contractServices.add('compileContracts', {
+        await this.contractServices.add('compileContracts', {
             date: Date.now(),
         });
     }
