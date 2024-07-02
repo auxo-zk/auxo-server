63107d572eb05839acfac1e29d042a37d94ce399
VN Phạm <nam.pv1805@gmail.com>
2024-07-02 21:56:25 +0700

    feat: add task api

--- src/apis/tasks/tasks.service.ts
+++ src/apis/tasks/tasks.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { GetTasksDto } from 'src/dtos/get-tasks.dto';
+import { DkgRequest } from 'src/schemas/request.schema';
 import { Task } from 'src/schemas/task.schema';
 
 @Injectable()
 export class TasksService {
     constructor(
         @InjectModel(Task.name) private readonly taskModel: Model<Task>,
+        @InjectModel(DkgRequest.name)
+        private readonly dkgRequestModel: Model<DkgRequest>,
     ) {}
 
     async getTasks(getTasksDto: GetTasksDto): Promise<Task[]> {
@@ -172,6 +175,21 @@ export class TasksService {
                     },
                 ]);
             }
+        }
+    }
+
+    async getRequest(taskId: number, requester: string): Promise<DkgRequest> {
+        try {
+            const task = await this.taskModel.findOne({
+                taskId: taskId,
+                requester: requester,
+            });
+            const request = await this.dkgRequestModel.findOne({
+                task: task.task,
+            });
+            return request;
+        } catch (err) {
+            throw new BadRequestException(err);
         }
     }
 }
