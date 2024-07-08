8fc8a3d9d5e6eb5b7ca8eb018a95ccdae6f76e8d
VN Phạm <nam.pv1805@gmail.com>
2024-07-08 12:15:22 +0700

    fix: wrong logics in handling get next resolve job

--- src/mina-contracts/dkg-usage-contracts/dkg-usage-contracts.service.ts
+++ src/mina-contracts/dkg-usage-contracts/dkg-usage-contracts.service.ts
@@ -833,7 +833,15 @@ export class DkgUsageContractsService implements ContractServiceInterface {
                 const committee = await this.committeeModel.findOne({
                     committeeId: key.committeeId,
                 });
-                if (request.responses.length == committee.threshold)
+                const existRequestAction = await this.requestActionModel.exists(
+                    {
+                        'actionData.requestId': request.requestId,
+                    },
+                );
+                if (
+                    request.responses.length == committee.threshold &&
+                    !existRequestAction
+                )
                     jobIds.push(`${request.requestId}`);
             }
         } catch (err) {
