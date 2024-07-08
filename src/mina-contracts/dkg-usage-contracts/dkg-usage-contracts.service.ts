a68f4e589c9a917ea394d68b72717432513e2344
VN Phạm <nam.pv1805@gmail.com>
2024-07-08 11:59:27 +0700

    fix: add unit for brute result

--- src/mina-contracts/dkg-usage-contracts/dkg-usage-contracts.service.ts
+++ src/mina-contracts/dkg-usage-contracts/dkg-usage-contracts.service.ts
@@ -875,6 +875,7 @@ export class DkgUsageContractsService implements ContractServiceInterface {
             const rawResultStorage = new ScalarVectorStorage();
             const rawResult = bruteForceResultVector(
                 getResultVector(totalD, totalM),
+                1,
             );
             const groupVectorStorage = new GroupVectorStorage();
             totalD.map((Di, index) => {
